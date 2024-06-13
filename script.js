1b55c35c15cb847cf921b7102cc981a23ca78cc7
biazinha32 <106695231+biazinha32@users.noreply.github.com>
2024-06-13 10:12:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const perguntas = [
                 afirmacao: "afirmação"
             },
             {
-                texto: "Elegante",
+                texto: "Despojado",
                 afirmacao: "afirmação"
             }
         ]
